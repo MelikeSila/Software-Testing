ecb295997adbd1998573c32f3a540877c7941fa8
Melike Keskin <melikekeskin@Melikes-MacBook-Pro.local>
2022-05-23 01:17:20 +0200

    char fixed

--- fuzzer.py
+++ fuzzer.py
@@ -1,16 +1,16 @@
-import string
 import random
 import tempfile
 
 def fuzz_generator(max_length=100, char_start=32, char_range=32):
     # TODO: Implement this function
-    chars = string.ascii_uppercase + "1" + "2" + "3" + "4"
     size = random.choice([max_length, int(max_length/2), int(max_length/4), int(max_length/5), int(max_length/6) ,int(max_length/7)])
-    return bytes(''.join(random.choice(chars) for _ in range(size)), 'UTF-8')
+    # size = randon.randrange(0, max_length+1)
+    return bytes(''.join(chr(random.randrange(char_start, char_start+char_range)) for _ in range(size)), 'UTF-8')
 
 # TODO: Implement fuzzer logic
 
 def fuzz():
+    print (fuzz_generator)
     """new_key = fuzz_generator()
                 fp = tempfile.TemporaryFile()
                 fp.write(new_key)
