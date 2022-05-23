4ee23b806005cb6c6e2821bfddb21e4164afc7fe
Melike Keskin <melikekeskin@Melikes-MacBook-Pro.local>
2022-05-24 00:20:33 +0200

    bc and uniq commands added

--- fuzzer.py
+++ fuzzer.py
@@ -1,38 +1,49 @@
 import random
 import tempfile
 
+
+###########################
+###########################
+
 def fuzz_generator(max_length=100, char_start=32, char_range=32):
     # TODO: Implement this function
     size = random.choice([max_length, int(max_length/2), int(max_length/4), int(max_length/5), int(max_length/6) ,int(max_length/7)])
     # size = randon.randrange(0, max_length+1)
-    return bytes(''.join(chr(random.randrange(char_start, char_start+char_range)) for _ in range(size)), 'UTF-8')
+    return "".join(chr(random.randrange(char_start, char_start+char_range)) for _ in range(size))
 
 # TODO: Implement fuzzer logic
 
-def fuzz():
-    print (fuzz_generator)
-    """new_key = fuzz_generator()
-                fp = tempfile.TemporaryFile()
-                fp.write(new_key)
-                # read data from file
-                fp.seek(0)
-                fp.read()
-            
-                # close the file, it will be removed
-                fp.close()"""
+##########################
+###########################
+import os
+import tempfile
 
-    # create a temporary file using a context manager
-    with tempfile.TemporaryFile() as fp:
-        fp.write(fuzz_generator())
-        fp.seek(0)
-        fp.read()
+def write(file, text, mode="w"):
+    with open(file, mode) as ff:
+        ff.write(text)
 
-    # file is now closed and removed
+"""file = os.path.join(tempfile.mkdtemp(), "fuzz_input.txt")
+print(file)"""
 
-    # create a temporary directory using the context manager
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        print('created temporary directory', tmpdirname)
-    # directory and contents have been removed
+###########################
+###########################
+
+import subprocess as sp
+def external_programs(program):
+    fuzz_input = fuzz_generator()
+    file = "fuzz_input.txt"
+    # store the fuzz input
+    write(file, fuzz_input)
+
+    #not inerested in stdin ve just interested in with stdout (standart output) and stderr(error channel).
+    # universal_newlines = True means output in text format
+    result = sp.run([program, file], stdin=sp.DEVNULL, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines = True)
+    return result
+
 
 if __name__ == "__main__":
-    fuzz()
+    print(external_programs("bc"))
+    print(external_programs("uniq"))
+    #print(external_programs("md5sum"))
+
+
